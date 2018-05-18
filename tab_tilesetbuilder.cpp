8ea0177fdae0cf7232ec54885b135781c5678328
comfykernel <comfykernel@gmail.com>
2018-05-17 19:37:41 -0700

    Trying with the tilemap rendering

--- tab_tilesetbuilder.cpp
+++ tab_tilesetbuilder.cpp
@@ -2,9 +2,53 @@
 #include "ui_tab_tilesetbuilder.h"
 
 #include <QXmlStreamReader>
+#include <QVBoxLayout>
 #include <QLineEdit>
 #include <QSpinBox>
+#include <QPainter>
 #include <QDebug>
+
+class CTilePreview : public QWidget {
+public:
+    QPen    pen;
+    QBrush  brush;
+    QPixmap pmx;
+    tab_tilesetbuilder* ttb;
+
+    CTilePreview(QWidget* parent = nullptr, tab_tilesetbuilder* tbuilder = nullptr)
+        : QWidget(parent){
+        ttb = tbuilder;
+
+        brush.setColor(Qt::GlobalColor::white);
+        brush.setStyle(Qt::BrushStyle::SolidPattern);
+
+        pen.setColor(Qt::GlobalColor::red);
+        pen.setStyle(Qt::PenStyle::SolidLine);
+        pen.setWidth(1);
+    }
+
+    void setPixmap(const QString& file) {
+        pmx.load(file);
+    }
+
+    void paintEvent(QPaintEvent*) {
+        qInfo()<<"Painting";
+
+        QPainter p(this);
+        p.setBrush(brush);
+        p.setPen  (pen);
+
+        p.drawPixmap(0, 0, pmx.width(), pmx.height(), pmx);
+
+        for(int x = 0; x < pmx.width() / ttb->info.width; ++x) {
+            for(int y = 0; y < pmx.height() / ttb->info.height; ++y) {
+                qInfo()<<"On square";
+                p.drawRect(QRect((x * ttb->info.width) + (ttb->info.spacing * x) + ttb->info.margin, (y * ttb->info.height) + (ttb->info.spacing * y) + ttb->info.margin,
+                                 ttb->info.width, ttb->info.height));
+            }
+        }
+    }
+};
 
 tab_tilesetbuilder::tab_tilesetbuilder(QWidget *parent) :
     CTabScreen(parent),
@@ -22,6 +66,15 @@ tab_tilesetbuilder::tab_tilesetbuilder(QWidget *parent) :
     connect(findChild<QSpinBox*>("sb_spacing"), &QSpinBox::editingFinished, this, &tab_tilesetbuilder::applyVisualSettings);
     connect(findChild<QSpinBox*>("sb_width"  ), &QSpinBox::editingFinished, this, &tab_tilesetbuilder::applyVisualSettings);
     connect(findChild<QSpinBox*>("sb_height" ), &QSpinBox::editingFinished, this, &tab_tilesetbuilder::applyVisualSettings);
+
+    QVBoxLayout* l = findChild<QVBoxLayout*>("vl_preview");
+
+    CTilePreview* ctp = new CTilePreview(this, this);
+    ctp->setObjectName("ctp_main");
+    ctp->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+    ctp->setMinimumSize(400, 400);
+    l->addWidget(ctp);
+    l->setAlignment(ctp, Qt::AlignHCenter);
 }
 
 const QList<QMenu*> tab_tilesetbuilder::menus(QWidget *parent) const {
@@ -65,6 +118,15 @@ void tab_tilesetbuilder::applyVisualSettings() {
     info.spacing = findChild<QSpinBox*>("sb_spacing")->value();
     info.width   = findChild<QSpinBox*>("sb_width"  )->value();
     info.height  = findChild<QSpinBox*>("sb_height" )->value();
+
+    QString cfile = _current_file;
+    cfile = cfile.remove(cfile.lastIndexOf('/') + 1, cfile.length() - cfile.lastIndexOf('/'));
+    cfile += texinfo.placeholder;
+    qInfo()<<"Setting pixmap : "<<cfile;
+
+    CTilePreview* ctp = findChild<CTilePreview*>("ctp_main");
+    ctp->setPixmap(cfile);
+    ctp->repaint();
 
     updateVisualInfo();
 }
